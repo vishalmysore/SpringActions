d9377e13220f6d048fb296c1323d05974619abdc
Vishal Row Mysore <visrow@gmail.com>
2024-03-22 08:53:43 -0400

    Spring , Java, Large Action Model

--- src/main/java/org/example/SAMController.java
+++ src/main/java/org/example/SAMController.java
@@ -3,6 +3,7 @@ package org.example;
 import com.t4a.predict.PredictionLoader;
 import com.t4a.processor.AIProcessingException;
 import com.t4a.processor.ActionProcessor;
+import com.t4a.processor.SpringAwareActionProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+/**
+ * There are no services instance variables defined here, Spring + Tools4AI will dynamically figure out what to call
+ * based on the prompt
+ */
 
 @RestController
 public class SAMController {
@@ -19,9 +22,7 @@ public class SAMController {
     private ApplicationContext applicationContext;
     @GetMapping("/action")
     public String actOnPrompt(@RequestParam("prompt") String prompt) {
-
-        PredictionLoader.getInstance(applicationContext);
-        ActionProcessor processor = new ActionProcessor();
+        SpringAwareActionProcessor processor = new SpringAwareActionProcessor(applicationContext);
         try {
             return (String) processor.processSingleAction(prompt);
         } catch (AIProcessingException e) {
@@ -32,9 +33,7 @@ public class SAMController {
 
     @PostMapping("/action")
     public String actOnPromptWithPost(  @RequestParam("car1") String car1,  @RequestParam("car2") String car2) {
-        System.out.println(applicationContext);
-        System.out.println(PredictionLoader.getInstance(applicationContext).getActionNameList());
-        ActionProcessor processor = new ActionProcessor();
+        SpringAwareActionProcessor processor = new SpringAwareActionProcessor(applicationContext);
         try {
             return (String) processor.processSingleAction(" This is car1 "+car1+" this is car2 "+car2);
         } catch (AIProcessingException e) {
--- src/main/java/org/example/SimpleService.java
+++ src/main/java/org/example/SimpleService.java
@@ -5,11 +5,11 @@ import com.t4a.predict.Predict;
 import org.springframework.stereotype.Service;
 
 @Service
-@Predict(actionName ="whatFoodDoesThisPersonLike", description = "Provide persons name and then find out what does that person like")
+@Predict(actionName ="whatThisPersonFavFood", description = "Provide persons name and then find out what does that person like")
 public class SimpleService implements JavaMethodAction {
 
 
-    public String whatFoodDoesThisPersonLike(String name) {
+    public String whatThisPersonFavFood(String name) {
         if("vishal".equalsIgnoreCase(name))
         return "Paneer Butter Masala";
         else if ("vinod".equalsIgnoreCase(name)) {
