8e805dc4894d9c03700338c5860b1c187c85c608
Vishal Row Mysore <visrow@gmail.com>
2025-05-17 12:52:54 -0400

    updated the versions and fixed the swagger now you can access the endpoints with http://localhost:7860/swagger-ui/index.html

--- src/main/java/io/github/vishalmysore/Application.java
+++ src/main/java/io/github/vishalmysore/Application.java
@@ -4,11 +4,7 @@ package io.github.vishalmysore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
