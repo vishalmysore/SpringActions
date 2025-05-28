d6e162f11ae4c65f8f2ef805d7487c66dbabc9cf
Vishal Row Mysore <visrow@gmail.com>
2025-05-28 17:30:20 -0400

    updated the versions and fixed the swagger now you can access the endpoints with http://localhost:7860/swagger-ui/index.html

--- src/main/java/io/github/vishalmysore/service/RaiseCustomerTicket.java
+++ src/main/java/io/github/vishalmysore/service/RaiseCustomerTicket.java
@@ -6,6 +6,7 @@ import com.t4a.annotations.Agent;
 import com.t4a.api.JavaMethodAction;
 
 import com.t4a.detect.ActionCallback;
+import com.t4a.detect.ActionState;
 import com.t4a.processor.AIProcessor;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
@@ -16,16 +17,28 @@ import org.springframework.stereotype.Service;
 public class RaiseCustomerTicket {
     /**
      * Each action has access to AIProcessor and ActionCallback which are autowired by tools4ai
+     * Use ThreadLocal to store the ActionCallback for the current thread if you are concerned
+     * about concurrency issues.
      */
-    private ActionCallback callback;
+    private static final ThreadLocal<ActionCallback> callbackThreadLocal = new ThreadLocal<>();
 
     /**
      * Each action has access to AIProcessor and ActionCallback which are autowired by tools4ai
      */
     private AIProcessor processor;
     @Action(description = "Raise a ticket for customer")
-    public String raiseTicket(String customerName) {
-
+    public String raiseTicket(String customerName, String reason) {
+        if(callbackThreadLocal.get() != null) {
+            ActionCallback callback = callbackThreadLocal.get();
+            log.info("callback is set "+callback);
+            // you can call the callback to get the AIProcessor
+            if(callback!=null) {
+                //the spelling mistake in the message is intentional to test the callback and the ai
+                callback.sendtStatus("raiseed ticket for will be resoved soon , t"+customerName+  " reason "+reason, ActionState.COMPLETED);
+            }
+        } else {
+            log.warning("callback is not set");
+        }
         return "ticket 111 raised for "+customerName;
     }
 }
